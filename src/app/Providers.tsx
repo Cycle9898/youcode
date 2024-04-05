36dd958771bf1cc10a71c1c5322bc857598bf1ee
Cycle9898 <cycle9898@gmail.com>
2024-04-05 19:58:50 +0200

    adding Tanstack React-Query and setting up its Provider component

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,17 +1,22 @@
 "use client";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 import { PropsWithChildren } from "react";
 import { Toaster } from "react-hot-toast";
 
+const queryClient = new QueryClient();
+
 export const Providers = ({ children }: PropsWithChildren) => {
 	return (
 		<>
-			<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-				<Toaster />
-				<SessionProvider>{children}</SessionProvider>
-			</ThemeProvider>
+			<QueryClientProvider client={queryClient}>
+				<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+					<Toaster />
+					<SessionProvider>{children}</SessionProvider>
+				</ThemeProvider>
+			</QueryClientProvider>
 		</>
 	);
 };
